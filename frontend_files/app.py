b720d5cf3c9b7db66c0492a651e3067a4b7763e3
Giles Bonner <gjbonner@gmail.com>
2026-08-17 11:54:21 -0400

    post request fix

--- frontend_files/app.py
+++ frontend_files/app.py
@@ -42,7 +42,7 @@ product_data = {
 
 if st.button("Predict"):
     response = requests.post("https://bug-free-space-carnival-q7gxr7949pj347q7-7860.app.github.dev/v1/predict", json=product_data)
-    if(response.status_code == 200):
+    if response.status_code == 200:
       result = response.json()
       predicted_sales = result["predicted_sales"]
       st.success(f"predicted Sales: {predicted_sales}")
